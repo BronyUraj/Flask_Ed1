01ec6f50eb3b228add47150abfbf127becd3ae5b
BronyUraj <ura.gudimov@gmail.com>
2021-01-19 04:45:47 -0500

    Last seen feature added

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm, RegistartionForm
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 @app.route("/")
@@ -63,3 +64,10 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [{"author": user, "body": "Test1"}, {"author": user, "body": "Test2"}]
     return render_template("user.html", user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
