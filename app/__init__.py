caae2e35b27c15f543fc730062926a44afa4cbde
BronyUraj <ura.gudimov@gmail.com>
2021-01-23 16:21:25 -0500

    Update with bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import RotatingFileHandler, SMTPHandler
 import os
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 mail = Mail(app)
+
 from app import routes, models, errors
 
 if not app.debug:
