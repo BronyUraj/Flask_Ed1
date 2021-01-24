3a229108c478329a1eddb1839f0a0d75aef4451f
BronyUraj <ura.gudimov@gmail.com>
2021-01-24 06:44:22 -0500

    Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import logging
 from logging.handlers import RotatingFileHandler, SMTPHandler
 import os
@@ -18,7 +19,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 mail = Mail(app)
-
+moment = Moment(app)
 from app import routes, models, errors
 
 if not app.debug:
